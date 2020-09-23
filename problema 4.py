df8fd8f4134dc6442a6d8e81bf80a217217390db
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2020-09-23 16:12:28 +0300

    Add files via upload

---
+++ problema 4.py
@@ -0,0 +1,6 @@
+n=4
+print(n,"metri","=",n*100,"centimetri")
+print(n,"kilograme","=",n*1000,"miligrame")
+print(n,"ani","=",n*12,"luni")
+print(n,"ani","=",n*52,"saptamani")
+print(n,"ani","=",n*365,"zile")
